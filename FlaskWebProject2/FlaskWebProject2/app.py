d60994c17c0e3fe23458158b0fa3330a7ccb26a7
ryutaro kamae <kamae_1116@yahoo.co.jp>
2018-10-25 01:48:59 +0900

    a

--- FlaskWebProject2/FlaskWebProject2/app.py
+++ FlaskWebProject2/FlaskWebProject2/app.py
@@ -4,11 +4,13 @@ It contains the definition of routes and views for the application.
 """
 
 from flask import Flask, request, render_template
+from bs4 import BeautifulSoup
+import urllib.request as req
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
-
+url = "http://api.aoikujira.com/kawase/xml/usd"
 
 @app.route("/")
 def hello():
@@ -18,10 +20,12 @@ def hello():
 
 @app.route("/result", methods = ["POST"])
 def result():
-    message = "This is paiza"
-    article = request.form["article"]
-    name = request.form["name"]
-    return render_template("form.html", message = message, article = article, name = name)
+	res = req.urlopen(url)
+	soup = BeautifulSoup(res, "html.parser")
+	jpy = soup.select_one("jpy").string
+	message = "This is paiza"
+	article = request.form["article"]
+	return render_template("form.html", message = message, article = article, jpy = jpy)
 
 if __name__ == '__main__':
     import os
